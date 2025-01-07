d04b1c500bc13c3a0e2fd1e6ca5ec52382db66b8
Gabriel-Kodehode <gabriel.kodehode@gmail.com>
2025-01-07 14:53:12 +0100

    fix matter.js

--- Rocket.js
+++ Rocket.js
@@ -9,9 +9,16 @@ const render = Matter.Render.create({
   },
 });
 
+var Bodies = Matter.Bodies;
+
 Matter.Render.run(render);
 
 const ball = Matter.Bodies.circle(400, 100, 40, { restitution: 0.9 });
 Matter.World.add(engine.world, [ball]);
+
+const ground = Matter.Bodies.rectangle(400, 400, 500, 30, { isStatic: true });
+Matter.World.add(engine.world, [ground]);
+
+Matter.Render.run(render);
 
 Matter.Runner.run(engine);
