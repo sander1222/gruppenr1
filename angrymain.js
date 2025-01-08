bfa6929e1ea5c1d94a7e1294e9c1933bb0c9e774
sander1222 <sander122.kodehode@gmail.com>
2025-01-08 09:46:53 +0100

    2

--- angrymain.js
+++ angrymain.js
@@ -1,20 +1,23 @@
 const { Composites } = require("matter-js");
 
-const { engine, Render, Runner, Bodies; composite } = Matter;
+const { Engine, Render, Runner, Bodies, Composite } = Matter;
 
-const engine =Engine.create();
+const engine = Engine.create();
 
 const render = Render.create({
-    element:  document.body,
-    engine: engine,
-    options: {
-        width: 800,
-        height: 600,
-        wireframes: false,
-    }
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+    wireframes: false,
+  },
 });
 
-const box = Bodies.rectangle()
+const box = Bodies.rectangle(400, 200, 80, 80);
+const ground = Bodies.rectangle(400, 500, 800, 50, { isStatic: true });
 
+Composite.add(engine.world, [box, ground]);
 
-Composite.add(engine.world, [box, round]);
+Render.run(render);
+Render.run(Runner.create(), engine);
